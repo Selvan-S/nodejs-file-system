406faf92bf8b3cb3ccaa9ccff19b49bda322685e
Selvan-S <selvan0023@gmail.com>
2024-05-13 21:36:18 +0530

    get back to old code

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const format = require("./utils/formatDate");
 const fs = require("fs");
-const os = require("os");
-const path = require("path");
-const desktopDir = path.join(os.homedir(), "Desktop");
-const timestamp_files_path = `${desktopDir}\\timestamp_files`;
+
 // Setting PORT
 const PORT = 8080;
 
@@ -16,8 +13,8 @@ app.get("/", (req, res) => {
     `<h1 style="text-align:center;">NodeJS File System</h1>
     <p>Try API enpoints:</p>
     <ul>
-      <li>/file/create</li>
-      <li>/file/read</li>
+      <li>/file/create (POST)</li>
+      <li>/file/read (GET)</li>
     </ul>`
   );
 });
@@ -27,15 +24,16 @@ app.post("/file/create", (req, res) => {
   const date = Date.now();
   const formatedDate = format.formatDate(date);
   const splitAndJoinDate = formatedDate.split(":").join(".");
-  if (!fs.existsSync(timestamp_files_path)) {
-    fs.mkdirSync(timestamp_files_path);
+  if (!fs.existsSync("./timestamp_files")) {
+    fs.mkdirSync("./timestamp_files");
   }
   fs.writeFile(
-    `${desktopDir}/timestamp_files/${splitAndJoinDate}.txt`,
+    `./timestamp_files/${splitAndJoinDate}.txt`,
     JSON.stringify(date),
     (err) => {
       if (err) {
         console.log("ERROR : ", err);
+        res.send(err);
       } else {
         console.log("Check!, new file is created");
       }
@@ -47,41 +45,40 @@ app.post("/file/create", (req, res) => {
 // Write API endpoint to retrieve all the text files in that particular folder.
 app.get("/file/read", (req, res) => {
   let fileTextJson = {};
-  if (!fs.existsSync(timestamp_files_path)) {
-    fs.mkdirSync(timestamp_files_path);
+  if (!fs.existsSync("./timestamp_files")) {
+    fs.mkdirSync("./timestamp_files");
   }
-  fs.readdir(`${desktopDir}/timestamp_files`, (err, files) => {
+  fs.readdir("./timestamp_files", (err, files) => {
     if (err) {
       console.log(
         "ERROR: There was an issue encountered while retrieving the files from the folder."
       );
+      res.send(err);
     } else {
       if (files.length == 0) {
-        res.send(
-          `<p>ERROR: No files were located in the directory ${timestamp_files_path}.</p><p>Use API endpoint of <code>/file/create</code> to create a new file.</p>`
-        );
-      } else {
-        files.forEach((file, index, arr) => {
-          fs.readFile(
-            `${desktopDir}/timestamp_files/${file}`,
-            "utf-8",
-            (err, data) => {
-              if (err) {
-                console.log(
-                  "ERROR: An error occurred while trying to access and read the files stored in the specified folder.",
-                  err
-                );
-              } else {
-                if (arr.length != index + 1) {
-                  fileTextJson[`${file}`] = data;
-                } else {
-                  res.send(fileTextJson);
-                }
-              }
-            }
-          );
+        res.send({
+          ERROR: `No files were located in the directory "./timestamp_files". Use API endpoint of "/file/create" which is an "POST" to create a new file.`,
         });
       }
+      files.forEach((file, index, arr) => {
+        fs.readFile(`./timestamp_files/${file}`, "utf-8", (err, data) => {
+          if (err) {
+            console.log(
+              "ERROR: An error occurred while trying to access and read the files stored in the specified folder.",
+              err
+            );
+            res.send(err);
+          } else {
+            if (arr.length != index + 1) {
+              fileTextJson[`${file}`] = data;
+            } else {
+              console.log("The file has been successfully read.");
+              fileTextJson[`${file}`] = data;
+              res.send(fileTextJson);
+            }
+          }
+        });
+      });
     }
   });
 });
